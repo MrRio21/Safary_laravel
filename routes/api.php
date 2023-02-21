81201197b8ae736df70945e591b3a4e8fa34595f
MrRio21 <mohussien223@gmsil.com>
2023-02-20 23:46:45 -0800

    xc

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+
+    // for all hotels review
+Route::get('/allotels',[HotelController::class,'index']);
+
+// data of the hotel that come from the form in the dashboard of hotel owner
+Route::post('/hotelRegisteration',[Hotel::class,'store']);
 });
