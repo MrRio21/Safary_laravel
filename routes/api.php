c2e33ae7b880764acc04e9e3ce9da5d81914954f
MrRio21 <mohussien223@gmsil.com>
2023-02-22 14:38:07 -0800

    solve_marina

--- routes/api.php
+++ routes/api.php
@@ -5,52 +5,39 @@ namespace App\Http\Controllers\HotelController;
 use App\Http\Controllers\HotelController;
 use App\Models\hotel;
 use Illuminate\Http\Request;
+use App\Http\Controllers\RequestRideController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Auth\registerationController\userController;
 use App\Http\Controllers\Api\Auth\registerationController\Drivercontroller;
 use App\Http\Controllers\Api\Auth\registerationController\HotelOwnerController;
 use App\Http\Controllers\Api\Auth\registerationController\TourgideController;
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-use App\Http\Controllers\RequestRideController;
-use App\Http\Controllers\Api\Auth\registerationController\userController;
-use App\Http\Controllers\Api\Auth\registerationController\Drivercontroller;
-use App\Http\Controllers\Api\Auth\registerationController\HotelOwnerController;
-use App\Http\Controllers\Api\Auth\registerationController\TourgideController;
+
+
+
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
 
-
+// Auth::routes();
 // //Driverregistrations---
 Route::get('/driverRegistrations',[App\Http\Controllers\Auth\reisteratinController\Drivercontroller :: class ,"index"]);
-// Route::get('/driverRegistrations/create',[App\Http\Controllers\Auth\reisteratinController\Drivercontroller :: class ,"create"]);
 Route::post('/driverRegistrations',[App\Http\Controllers\Auth\reisteratinController\Drivercontroller :: class ,"store"]);
 // //Userregistrations---
 Route::get('/userRegistrations',[App\Http\Controllers\Api\Auth\registerationController\userController :: class ,"index"]);
-// Route::get('/userRegistrations/create',[App\Http\Controllers\Auth\reisteratinController\userController :: class ,"create"]);
 Route::post('/userRegistrations',[App\Http\Controllers\Auth\reisteratinController\userController :: class ,"store"]);
 // //hotelOwnerRegistrations---
 Route::get('/hotelOwnerRegistrations',[App\Http\Controllers\Auth\reisteratinController\HotelOwnerController :: class ,"index"]);
-// Route::get('/hotelOwnerRegistrations/create',[App\Http\Controllers\Auth\reisteratinController\HotelOwnerController :: class ,"create"]);
-Route::post('/hotelOwnerRegistrations',[App\Http\Controllers\Auth\reisteratinController\HotelOwnerController :: class ,"store"]); 
+Route::post('/hotelOwnerRegistrations',[App\Http\Controllers\Auth\reisteratinController\HotelOwnerController :: class ,"store"]);
 // //TourgideRegistrations---
 Route::get('/tourgideRegistrations',[App\Http\Controllers\Auth\reisteratinController\TourgideController :: class ,"index"]);
-// Route::get('/tourgideRegistrations/create',[App\Http\Controllers\Auth\reisteratinController\TourgideController :: class ,"create"]);
 Route::post('/tourgideRegistrations',[App\Http\Controllers\Auth\reisteratinController\TourgideController :: class ,"store"]);
 
 // Route::group(['middleware' => ['web']], function () {
 //     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('auth');
-    
+
 // });
 
 
-// Auth::routes();
-
-
-
-// Auth::routes();
 
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
@@ -84,30 +71,6 @@ Route::get('/updateHotel{id}',[HotelController::class,'Update']);
 
 
 
-
-// //Driverregistrations---
-Route::get('/driverRegistrations',[App\Http\Controllers\Auth\reisteratinController\Drivercontroller :: class ,"index"]);
-// Route::get('/driverRegistrations/create',[App\Http\Controllers\Auth\reisteratinController\Drivercontroller :: class ,"create"]);
-Route::post('/driverRegistrations',[App\Http\Controllers\Auth\reisteratinController\Drivercontroller :: class ,"store"]);
-// //Userregistrations---
-Route::get('/userRegistrations',[App\Http\Controllers\Api\Auth\registerationController\userController :: class ,"index"]);
-// Route::get('/userRegistrations/create',[App\Http\Controllers\Auth\reisteratinController\userController :: class ,"create"]);
-Route::post('/userRegistrations',[App\Http\Controllers\Auth\reisteratinController\userController :: class ,"store"]);
-// //hotelOwnerRegistrations---
-Route::get('/hotelOwnerRegistrations',[App\Http\Controllers\Auth\reisteratinController\HotelOwnerController :: class ,"index"]);
-// Route::get('/hotelOwnerRegistrations/create',[App\Http\Controllers\Auth\reisteratinController\HotelOwnerController :: class ,"create"]);
-Route::post('/hotelOwnerRegistrations',[App\Http\Controllers\Auth\reisteratinController\HotelOwnerController :: class ,"store"]); 
-// //TourgideRegistrations---
-Route::get('/tourgideRegistrations',[App\Http\Controllers\Auth\reisteratinController\TourgideController :: class ,"index"]);
-// Route::get('/tourgideRegistrations/create',[App\Http\Controllers\Auth\reisteratinController\TourgideController :: class ,"create"]);
-Route::post('/tourgideRegistrations',[App\Http\Controllers\Auth\reisteratinController\TourgideController :: class ,"store"]);
-
-// Route::group(['middleware' => ['web']], function () {
-//     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('auth');
-    
-// });
-
-
 // Auth::routes();
 
 
@@ -115,4 +78,3 @@ Route::post('/tourgideRegistrations',[App\Http\Controllers\Auth\reisteratinContr
 // Auth::routes();
 
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
