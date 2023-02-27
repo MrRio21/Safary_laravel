4e6729a10f256bb536cbfaa40dabf28456f866a4
AnfalMohamedIbrahimAhmed <anfal.mohamed38@gmail.com>
2023-02-28 01:00:37 +0200

    Anfal

--- app/Models/OrderDetail.php
+++ app/Models/OrderDetail.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class OrderDetail extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'order_id',
+        'room_status',
+        'tourGuide_status',
+        'total_price',
+    ];
+    public function User()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function Order()
+    {
+        return $this->hasMany(Order::class);
+    }
+
 }
