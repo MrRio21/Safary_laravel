fcf3e9d978c2f4229057baa09e25350b092e9a30
Marina Ayman <99688015+marina-ayman@users.noreply.github.com>
2023-03-08 03:07:30 -0800

    blade

---
+++ resources/views/payment.blade.php
@@ -0,0 +1,231 @@
+
+
+<div class="container m-5 mt-4 p-0">
+    <nav class="navbar navbar-expand-lg navbar-light bg-white pt-3 px-md-4 px-2">
+        <div class="container-fluid"> <a class="navbar-brand" href="#">clothing</a> <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav" aria-controls="nav" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
+            <div class="collapse navbar-collapse" id="nav">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item"> <a class="nav-link active" href="#">New</a> </li>
+                    <li class="nav-item"> <a class="nav-link" href="#">Men</a> </li>
+                    <li class="nav-item"> <a class="nav-link" href="#">Women</a> </li>
+                    <li class="nav-item"> <a class="nav-link" href="#">Kids</a> </li>
+                    <li class="nav-item"> <a class="nav-link" href="#">Special Offers</a> </li>
+                </ul>
+                <ul class="d-flex mb-0">
+                    <li class="nav-item pe-3">
+                        <div class="d-flex align-items-center">
+                            <div class="icon me-3"> <span class="fas fa-shopping-cart"></span> </div>
+                            <div class="cart">
+                                <p class="">My cart</p> <small class="">3 items</small>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="nav-item me-3">
+                        <div class="btn btn-outline-primary">Sign Up</div>
+                    </li>
+                    <li class="nav-item">
+                        <div class="btn btn-primary">Login</div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="row px-md-4 px-2 pt-4">
+        <div class="col-lg-8">
+            <p class="pb-2 fw-bold">Order</p>
+            <div class="card">
+                <div class="ribbon ribbon-top-right"><span>SALE TIME!</span></div>
+                <div>
+                    <div class="table-responsive px-md-4 px-2 pt-3">
+                        <table class="table table-borderless">
+                            <tbody>
+                                <tr class="border-bottom">
+                                    <td>
+                                        <div class="d-flex align-items-center">
+                                            <div> <img class="pic" src="https://images.pexels.com/photos/7322083/pexels-photo-7322083.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt=""> </div>
+                                            <div class="ps-3 d-flex flex-column justify-content">
+                                                <p class="fw-bold">Air<span class="ps-1">Red</span><span class="ps-1">Pants</span></p> <small class=" d-flex"> <span class=" text-muted">Color:</span> <span class=" fw-bold">Red/White</span> </small> <small class=""> <span class=" text-muted">Size:</span> <span class=" fw-bold">L</span> </small>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div class="d-flex">
+                                            <p class="pe-3"><span class="red">$45.00</span></p>
+                                            <p class="text-muted text-decoration-line-through">$55.00</p>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div class="d-flex align-items-center"> <span class="pe-3 text-muted">Quantity</span> <span class="pe-3"> <input class="ps-2" type="number" value="2"></span>
+                                            <div class="round"> <span class=""> L </span> </div>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr class="border-bottom">
+                                    <td>
+                                        <div class="d-flex align-items-center">
+                                            <div> <img class="pic" src="https://images.pexels.com/photos/7322083/pexels-photo-7322083.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt=""> </div>
+                                            <div class="ps-3 d-flex flex-column">
+                                                <p class="fw-bold">Court<span class="ps-1">Dri-FIT</span></p> <small class=" d-flex"> <span class=" text-muted">Color:</span> <span class="fw-bold">Dark Blue</span> </small> <small class=""> <span class=" text-muted">Size:</span> <span class="fw-bold">L</span> </small>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div class="d-flex">
+                                            <p class="pe-3"><span class="red">$45.00</span></p>
+                                            <p class="text-muted text-decoration-line-through">$55.00</p>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div class="d-flex align-items-center"> <span class="pe-3 text-muted">Quantity</span> <span class="pe-3"> <input class="ps-2" type="number" value="2"></span>
+                                            <div class="round"> <span class=""> L </span> </div>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr class="">
+                                    <td>
+                                        <div class="d-flex align-items-center">
+                                            <div> <img class="pic" src="https://images.pexels.com/photos/7322083/pexels-photo-7322083.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt=""> </div>
+                                            <div class="ps-3 d-flex flex-column">
+                                                <p class="fw-bold"> Sportswear<span class="ps-1">Heritage</span><span class="ps-1">Windrunner</span></p> <small class=""> <span class=" text-muted">Color:</span> <span class="fw-bold">Blue/White</span> </small> <small class=""> <span class=" text-muted">Size:</span> <span class="fw-bold">L</span> </small>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div class="d-flex">
+                                            <p class="pe-3"><span class="red">$45.00</span></p>
+                                            <p class="text-muted text-decoration-line-through">$55.00</p>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div class="d-flex align-items-center"> <span class="pe-3 text-muted">Quantity</span> <span class="pe-3"> <input class="ps-2" type="number" value="2"></span>
+                                            <div class="round"> <span class=""> L </span> </div>
+                                        </div>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4 payment-summary">
+            <p class="fw-bold pt-lg-0 pt-4 pb-2">Payment Summary</p>
+            <div class="card px-md-3 px-2 pt-4">
+                <div class="unregistered mb-4"> <span class="py-1">unregistered account</span> </div>
+                <div class="d-flex justify-content-between pb-3"> <small class="text-muted">Transaction code</small>
+                    <p class="">VC115665</p>
+                </div>
+                <div class="d-flex justify-content-between b-bottom"> <input type="text" class="ps-2" placeholder="COUPON CODE">
+                    <div class="btn btn-primary">Apply</div>
+                </div>
+                <div class="d-flex flex-column b-bottom">
+                    <div class="d-flex justify-content-between py-3"> <small class="text-muted">Order Summary</small>
+                        <p>$122</p>
+                    </div>
+                    <div class="d-flex justify-content-between pb-3"> <small class="text-muted">Additional Service</small>
+                        <p>$22</p>
+                    </div>
+                    <div class="d-flex justify-content-between"> <small class="text-muted">Total Amount</small>
+                        <p>$132</p>
+                    </div>
+                </div>
+                <div class="sale my-3"> <span>sale<span class="px-1">expiring</span><span>in</span>:</span><span class="red">21<span class="ps-1">hours</span>,31<span class="ps-1 ">minutes</span></span> </div>
+            </div>
+        </div>
+        <div class="col-lg-8 delivery px-md-3 px-1">
+            <p class="pt-4 fw-bold pb-3 ps-2">Delivery</p>
+            <div class="d-md-flex justify-content-between align-items-center pb-4">
+                <div class="size d-flex ps-4"> <label class="option d-flex align-items-center"> <input type="radio" name="radio">
+                        <div class="card">
+                            <div class="d-flex align-items-center">
+                                <div class="d-flex align-items-center justify-content-center"> <img class="del" src="https://www.freepnglogos.com/uploads/ups-logo-transparent/ups-airlines-aviation-22.png" alt="">
+                                    <p class="ps-2">DPD</p>
+                                </div>
+                                <div class=" d-flex flex-column px-3">
+                                    <p class="fw-bold pb-1">DPD<span>Deliveries</span></p> <small class="text-muted pb-1">Expected<span class="ps-1">delivery:</span></small> <small class="text-muted pb-1">Friday:28</small>
+                                </div>
+                                <p class="fw-bold">$5.25</p>
+                            </div>
+                        </div> <span class="checkmark"></span>
+                    </label> </div>
+                <div class="size d-flex pt-md-0 pt-3 ps-md-0 ps-4"> <label class="option d-flex align-items-center"> <input type="radio" name="radio">
+                        <div class="card">
+                            <div class="d-flex align-items-center">
+                                <div class="d-flex align-items-center justify-content-center"> <img class="del" src="https://www.freepnglogos.com/uploads/ups-logo-transparent/ups-airlines-aviation-22.png" alt="">
+                                    <p class="ps-2">DPD</p>
+                                </div>
+                                <div class=" d-flex flex-column px-3">
+                                    <p class="fw-bold pb-1">DPD<span>Deliveries</span></p> <small class="text-muted pb-1">Expected<span class="ps-1">delivery:</span></small> <small class="text-muted pb-1">Friday:28</small>
+                                </div>
+                                <p class="fw-bold">$5.25</p>
+                            </div>
+                        </div> <span class="checkmark"></span>
+                    </label> </div>
+            </div>
+            <div class="d-md-flex justify-content-between align-items-center pb-4">
+                <div class="size d-flex ps-4"> <label class="option d-flex align-items-center"> <input type="radio" name="radio">
+                        <div class="card">
+                            <div class="d-flex align-items-center">
+                                <div class="d-flex align-items-center justify-content-center"> <img class="del" src="https://www.freepnglogos.com/uploads/ups-logo-transparent/ups-airlines-aviation-22.png" alt="">
+                                    <p class="ps-2">DPD</p>
+                                </div>
+                                <div class=" d-flex flex-column px-3">
+                                    <p class="fw-bold pb-1">DPD<span>Deliveries</span></p> <small class="text-muted pb-1">Expected<span class="ps-1">delivery:</span></small> <small class="text-muted pb-1">Friday:28</small>
+                                </div>
+                                <p class="fw-bold">$5.25</p>
+                            </div>
+                        </div> <span class="checkmark"></span>
+                    </label> </div>
+                <div class="size d-flex pt-md-0 pt-3 ps-md-0 ps-4"> <label class="option d-flex align-items-center"> <input type="radio" name="radio">
+                        <div class="card">
+                            <div class="d-flex align-items-center">
+                                <div class="d-flex align-items-center justify-content-center"> <img class="del" src="https://www.freepnglogos.com/uploads/ups-logo-transparent/ups-airlines-aviation-22.png" alt="">
+                                    <p class="ps-2">DPD</p>
+                                </div>
+                                <div class=" d-flex flex-column px-3">
+                                    <p class="fw-bold pb-1">DPD<span>Deliveries</span></p> <small class="text-muted pb-1">Expected<span class="ps-1">delivery:</span></small> <small class="text-muted pb-1">Friday:28</small>
+                                </div>
+                                <p class="fw-bold">$5.25</p>
+                            </div>
+                        </div> <span class="checkmark"></span>
+                    </label> </div>
+            </div>
+        </div>
+        <div class="col-lg-4">
+            <p class="pt-4 fw-bold pb-3">Additional Service</p>
+            <div class="card p-3 mb-2">
+                <div class="d-flex align-items-center justify-content-between">
+                    <div>
+                        <p class="fw-bold">Care + package</p> <small class="text-muted">One year of additional Care</small>
+                    </div>
+                    <div class="d-flex align-items-center">
+                        <p class="pe-3">$10.00</p>
+                        <div class="form-check form-switch"> <input class="form-check-input" type="checkbox" id="SwitchCheck"> </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card p-3 mb-2">
+                <div class="d-flex align-items-center justify-content-between">
+                    <div>
+                        <p class="fw-bold">Environment Friendly</p> <small class="text-muted">Add some tip for earth care</small>
+                    </div>
+                    <div class="d-flex align-items-center">
+                        <p class="pe-3">$2.00</p>
+                        <div class="form-check form-switch"> <input class="form-check-input" type="checkbox" id="SwitchCheck"> </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card p-3 mb-2">
+                <div class="d-flex align-items-center justify-content-between">
+                    <div>
+                        <p class="fw-bold">Golden Guard</p> <small class="text-muted">30 days more for return</small>
+                    </div>
+                    <div class="d-flex align-items-center">
+                        <p class="pe-3">$5.00</p>
+                        <div class="form-check form-switch"> <input class="form-check-input" type="checkbox" id="SwitchCheck"> </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
