1eeb82003d784b741c543162663a47c577221417
AhmedObaida <ahmedobaida707@gmail.com>
2023-03-10 15:06:44 +0200

    commit obaida

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ use AApp\Http\Controllers\Auth\registerationControlle\HotelOwnerController;
 use AApp\Http\Controllers\Auth\registerationControlle\TourgideController;
 use App\Http\Controllers\Auth\registerationControlle\userController;
 use App\Http\Controllers\orderController\OrderController;
+use App\Http\Controllers\tripController\ChosenTripController;
 
 /*
 |--------------------------------------------------------------------------
@@ -42,6 +43,7 @@ Route::get('/', function () {
 // Route::get('/tourgideRegistrations/create',[TourgideController :: class ,"create"])->name("tourgideRegistrations.create");
 // Route::post('/tourgideRegistrations',[TourgideController :: class ,"store"])->name("tourgideRegistrations.store");
 
+Route::get('testtrip',[ChosenTripController :: class, 'test']);
 
 Route::group(['middleware' => ['web']], function () {
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('auth');
@@ -62,6 +64,10 @@ Route::get('/test',function(){
 });
 
 Route::get('/hotels',[HotelController::class,'index']);
+Route::get('/test',function(){
+    return view ('dashboard/driver');
+});
+
 Route::get('/order',[OrderController::class,'index']);
 // Route::post('/order',[OrderController::class,'store']);
 
